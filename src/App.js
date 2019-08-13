c97d522d04ace198fa99c8f8b421021c5160a90e
james-zedd <james_zedd@hotmail.com>
2019-08-12 21:13:14 -0400

    project production changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
+import User from './components/users/User';
+import Alert from './components/layout/Alert';
+import About from './components/pages/About';
 
-class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <Navbar />
-        <div className='container'>
-          <Users />
-        </div>
-      </div>
-    );
-  }
-}
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
+
+import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
+
+const App = () => {
+  return (
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className='App'>
+            <Navbar />
+            <div className='container'>
+              <Alert />
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
+  );
+};
 
 export default App;
